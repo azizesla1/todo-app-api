10d1cb33f2fa014374f5cc2bde92a670935fec67
azizesla1 <slamaniabdelazize16@gmail.com>
2024-05-11 13:12:28 +0100

    add comments

--- index.js
+++ index.js
@@ -1,29 +1,52 @@
+// Import the express module
 const express = require("express");
+// Create an Express application
 const app = express();
+// Import utility functions for task management
 const { tasks, addTask, removeTask, updateTask } = require("./utils");
+
+// Middleware to parse JSON bodies
 app.use(express.json());
+
+// Route to get all tasks
 app.get("/tasks", (req, res) => {
+  // Respond with the current list of tasks in JSON format
   res.status(200).json(tasks);
 });
+
+// Route to add a new task
 app.post("/task", (req, res) => {
-  console.log("hiiii");
+  // Extract the new task from the request body
   const newTask = req.body;
+  // Add the new task to the list
   addTask(newTask);
-  res.status(200).send("tasks");
-});
-app.put("/task/:taskId", (req, res) => {
-  const id = req.params.taskId;
-  const givenTask = req.body;
-  updateTask(id, givenTask);
-  res.status(200).send("tasks updated");
+  // Respond to the client
+  res.status(200).send("Task added");
 });
 
-app.delete("/task/:taskDee", (req, res) => {
-  const id = req.params.taskDee;
-  console.log(id);
-  removeTask(id);
-  res.status(200).send("task removed");
+// Route to update an existing task
+app.put("/task/:taskId", (req, res) => {
+  // Extract the task ID from the URL parameters
+  const id = req.params.taskId;
+  // Extract the updated task data from the request body
+  const givenTask = req.body;
+  // Update the task in the list
+  updateTask(id, givenTask);
+  // Respond to the client
+  res.status(200).send("Task updated");
 });
+
+// Route to delete a task
+app.delete("/task/:taskDee", (req, res) => {
+  // Extract the task ID from the URL parameters
+  const id = req.params.taskDee;
+  // Remove the task from the list
+  removeTask(id);
+  // Respond to the client
+  res.status(200).send("Task removed");
+});
+
+// Start the server on port 3004
 app.listen(3004, () => {
-  console.log("server is running on port 3004");
+  console.log("Server is running on port 3004");
 });
